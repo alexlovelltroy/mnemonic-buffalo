3cd9ce096ad31cd777d7f32633125f0f32b10a88
Alex Lovell-Troy <alex@lovelltroy.org>
2018-03-18 21:54:38 -1000

    Added ntc and a color card

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,5 +1,11 @@
 require("expose-loader?$!expose-loader?jQuery!jquery");
 require("bootstrap-sass/assets/javascripts/bootstrap.js");
 $(() => {
+    var n_match  = ntc.name(hexcolor);
+    var n_rgb        = n_match[0]; // This is the RGB value of the closest matching color
+    var n_name       = n_match[1]; // This is the text string for the name of the match
+    var n_exactmatch = n_match[2]; // True if exact color match, False if close-match
 
+    // alert(n_name);
+    $( "span.hexcolor" ).html( n_name );
 });
